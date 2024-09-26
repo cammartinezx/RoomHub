b8a431999321258db218f887763021ef02582a7e
DannyFabs <aderemifawoyed@gmail.com>
2024-09-25 22:30:32 -0500

    added basic logic to userinfo handler

--- Backend/src/Handler/UserInfoHandler.js
+++ Backend/src/Handler/UserInfoHandler.js
@@ -4,11 +4,34 @@ class UserInfoHandler {
     #user_persistence;
 
     constructor() {
-        this.#user_persistence = Services.getUserPersistence();
+        this.#user_persistence = Services.get_user_persistence();
     }
 
     get_user_persistence() {
         return this.#user_persistence;
+    }
+
+    #is_valid_id(user_id) {
+        if (user_id === "") {
+            return false;
+        }
+        return true;
+    }
+
+    async create_user(request, response) {
+        const user_id = request.params.id;
+
+        if (!this.#is_valid_id(user_id)) {
+            // give a certain type of response
+            response.status(400).json({ message: "Error Creating User- User id is invalid" });
+        }
+
+        try {
+            let result = await this.#user_persistence.save_new_user(user_id);
+            response.status(result.status).json({ message: result.message });
+        } catch (error) {
+            response.status(500).json({ message: error });
+        }
     }
 }
 
---
+++ Backend/src/router/User.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const UserInfoHandler = require("../Handler/UserInfoHandler");
+
+const user_info_handler = new UserInfoHandler();
+
+router.get("/:id", (req, res) => {
+    user_info_handler.create_user(req, res);
+});
