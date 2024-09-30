6dfb9c960dd6795b90685dc1ae4dfe3734589528
cammartinezx <camila.martinez2002@gmail.com>
2024-09-30 15:35:24 -0500

    set up

---
+++ flutter_frontend/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_frontend/screens/login/login.dart';
+import 'package:flutter_frontend/utils/our_theme.dart';
+void main() => runApp (const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  //• This widget-is-the-root of your application.
+  @override
+  Widget build(BuildContext context){
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: OurTheme().buildTheme(),
+    home: const OurLogin(),
+    
+  );
+}
+}
+
+
