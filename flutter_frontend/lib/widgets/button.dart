0c36b7c446e0adb49a1d621f1392f2faa9c452b5
cammartinezx <camila.martinez2002@gmail.com>
2024-10-03 08:07:27 -0500

    button template

---
+++ flutter_frontend/lib/widgets/button.dart
@@ -0,0 +1,97 @@
+/*import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String text; 
+  final void Function()? onTap;
+  const MyButton({super.key, required this.text, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).secondaryHeaderColor,
+          borderRadius: BorderRadius.circular(40.0),
+        ), // BoxDecoration
+        padding: const EdgeInsets.all(13),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(text, style: const TextStyle(color: Colors.white)), // Text
+            const SizedBox(width: 10),
+            const Icon(Icons.arrow_forward, color: Colors.white)
+          ],
+        ),
+      ),
+    );
+  }
+}*/
+import 'package:flutter/material.dart';
+
+class MyButton extends StatefulWidget {
+  final String text;
+  final void Function()? onTap;
+
+  const MyButton({super.key, required this.text, required this.onTap});
+
+  @override
+  MyButtonState createState() => MyButtonState();
+}
+
+class MyButtonState extends State<MyButton> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    );
+
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 0.90).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) => _controller.forward(),
+      onTapUp: (_) {
+        _controller.reverse();
+        widget.onTap?.call(); // Call the onTap function
+      },
+      onTapCancel: () => _controller.reverse(),
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).secondaryHeaderColor.withOpacity(0.5),
+            borderRadius: BorderRadius.circular(40.0),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 30) + const EdgeInsets.only(top: 13, bottom: 13), // Adjust horizontal and vertical padding
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+             mainAxisSize: MainAxisSize.min, 
+            children: [
+              Text(
+                widget.text,
+                style: const TextStyle(color: Colors.white),
+              ), // Text
+              const SizedBox(width: 10),
+              const Icon(Icons.arrow_forward, color: Colors.white),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
