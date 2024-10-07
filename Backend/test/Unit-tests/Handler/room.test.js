f864b3c444d1a7de349c2874a97c2a15dab4a326
DannyFabs <aderemifawoyed@gmail.com>
2024-10-07 09:22:16 -0500

    removed comments

--- Backend/test/Unit-tests/Handler/room.test.js
+++ Backend/test/Unit-tests/Handler/room.test.js
@@ -14,110 +14,110 @@ jest.mock("../../../src/Utility/Services", () => ({
     }),
 }));
 
-// describe("Unit test for creating room", () => {
-//     let roomHandler;
-//     let req, res;
+describe("Unit test for creating room", () => {
+    let roomHandler;
+    let req, res;
 
-//     beforeEach(() => {
-//         roomHandler = new RoomHandler();
-//         req = mockRequest();
-//         res = mockResponse();
+    beforeEach(() => {
+        roomHandler = new RoomHandler();
+        req = mockRequest();
+        res = mockResponse();
 
-//         res.status = jest.fn().mockReturnValue(res); // Allows chaining like res.status(200).json()
-//         res.json = jest.fn();
-//         jest.clearAllMocks();
-//     });
+        res.status = jest.fn().mockReturnValue(res); // Allows chaining like res.status(200).json()
+        res.json = jest.fn();
+        jest.clearAllMocks();
+    });
 
-//     it("Send a success response verifying that the room was correctly created", async () => {
-//         // mock get_user and get_room_name
-//         roomHandler.get_user_persistence().get_user.mockImplementation(() => {
-//             return { user_id: "test@gmail.com", room_id: "111-111" };
-//         });
-//         roomHandler.get_room_persistence().generate_new_room.mockImplementation((room_id, room_name, user_id) => {
-//             return "SUCCESS";
-//         });
+    it("Send a success response verifying that the room was correctly created", async () => {
+        // mock get_user and get_room_name
+        roomHandler.get_user_persistence().get_user.mockImplementation(() => {
+            return { user_id: "test@gmail.com", room_id: "111-111" };
+        });
+        roomHandler.get_room_persistence().generate_new_room.mockImplementation((room_id, room_name, user_id) => {
+            return "SUCCESS";
+        });
 
-//         req.body = { id: "test@gmail.com", rm: "1000" };
+        req.body = { id: "test@gmail.com", rm: "1000" };
 
-//         await roomHandler.create_room(req, res);
+        await roomHandler.create_room(req, res);
 
-//         expect(res.status).toHaveBeenCalledWith(200);
-//         expect(res.json).toHaveBeenCalledWith({ message: "Successfully Created the new room" });
-//     });
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({ message: "Successfully Created the new room" });
+    });
 
-//     it("Send a response signifying that the room was not created", async () => {
-//         // mock get_user and get_room_name
-//         roomHandler.get_user_persistence().get_user.mockImplementation(() => {
-//             return { user_id: "test@gmail.com", room_id: "111-111" };
-//         });
-//         roomHandler.get_room_persistence().generate_new_room.mockImplementation((room_id, room_name, user_id) => {
-//             return "FAILURE";
-//         });
+    it("Send a response signifying that the room was not created", async () => {
+        // mock get_user and get_room_name
+        roomHandler.get_user_persistence().get_user.mockImplementation(() => {
+            return { user_id: "test@gmail.com", room_id: "111-111" };
+        });
+        roomHandler.get_room_persistence().generate_new_room.mockImplementation((room_id, room_name, user_id) => {
+            return "FAILURE";
+        });
 
-//         req.body = { id: "test@gmail.com", rm: "1000" };
+        req.body = { id: "test@gmail.com", rm: "1000" };
 
-//         await roomHandler.create_room(req, res);
+        await roomHandler.create_room(req, res);
 
-//         expect(res.status).toHaveBeenCalledWith(500);
-//         expect(res.json).toHaveBeenCalledWith({ message: "Retry creating the room" });
-//     });
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith({ message: "Retry creating the room" });
+    });
 
-//     it("Send a response signifying that the user name is invalid", async () => {
-//         // mock get_user and get_room_name
-//         roomHandler.get_user_persistence().get_user.mockImplementation(() => {
-//             return null;
-//         });
+    it("Send a response signifying that the user name is invalid", async () => {
+        // mock get_user and get_room_name
+        roomHandler.get_user_persistence().get_user.mockImplementation(() => {
+            return null;
+        });
 
-//         req.body = { id: "test@gmail.com", rm: "1000" };
+        req.body = { id: "test@gmail.com", rm: "1000" };
 
-//         await roomHandler.create_room(req, res);
+        await roomHandler.create_room(req, res);
 
-//         expect(res.status).toHaveBeenCalledWith(400);
-//         expect(res.json).toHaveBeenCalledWith({ message: "Bad Request-Invalid User" });
-//     });
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ message: "Bad Request-Invalid User" });
+    });
 
-//     it("Send a response signifying that the room name is invalid", async () => {
-//         // mock get_user and get_room_name
-//         roomHandler.get_user_persistence().get_user.mockImplementation(() => {
-//             return { user_id: "test@gmail.com", room_id: "111-111" };
-//         });
+    it("Send a response signifying that the room name is invalid", async () => {
+        // mock get_user and get_room_name
+        roomHandler.get_user_persistence().get_user.mockImplementation(() => {
+            return { user_id: "test@gmail.com", room_id: "111-111" };
+        });
 
-//         req.body = { id: "test@gmail.com", rm: "" };
+        req.body = { id: "test@gmail.com", rm: "" };
 
-//         await roomHandler.create_room(req, res);
+        await roomHandler.create_room(req, res);
 
-//         expect(res.status).toHaveBeenCalledWith(400);
-//         expect(res.json).toHaveBeenCalledWith({ message: "Bad Request-Invalid Room Name" });
-//     });
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ message: "Bad Request-Invalid Room Name" });
+    });
 
-//     it("Send a response signifying that the room name and user name are invalid", async () => {
-//         // mock get_user and get_room_name
-//         roomHandler.get_user_persistence().get_user.mockImplementation(() => {
-//             return null;
-//         });
+    it("Send a response signifying that the room name and user name are invalid", async () => {
+        // mock get_user and get_room_name
+        roomHandler.get_user_persistence().get_user.mockImplementation(() => {
+            return null;
+        });
 
-//         req.body = { id: "test@gmail.com", rm: "" };
+        req.body = { id: "test@gmail.com", rm: "" };
 
-//         await roomHandler.create_room(req, res);
+        await roomHandler.create_room(req, res);
 
-//         expect(res.status).toHaveBeenCalledWith(400);
-//         expect(res.json).toHaveBeenCalledWith({ message: "Bad Request-Invalid User and room name" });
-//     });
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ message: "Bad Request-Invalid User and room name" });
+    });
 
-//     it("Send a response signifying that some other error has been thrown from database", async () => {
-//         // mock get_user and get_room_name
-//         roomHandler.get_user_persistence().get_user.mockImplementation(() => {
-//             throw new Error("Sample db error");
-//         });
+    it("Send a response signifying that some other error has been thrown from database", async () => {
+        // mock get_user and get_room_name
+        roomHandler.get_user_persistence().get_user.mockImplementation(() => {
+            throw new Error("Sample db error");
+        });
 
-//         req.body = { id: "test@gmail.com", rm: "" };
+        req.body = { id: "test@gmail.com", rm: "" };
 
-//         await roomHandler.create_room(req, res);
+        await roomHandler.create_room(req, res);
 
-//         expect(res.status).toHaveBeenCalledWith(500);
-//         expect(res.json).toHaveBeenCalledWith(expect.any(Object));
-//     });
-// });
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith(expect.any(Object));
+    });
+});
 
 describe("Unit test for adding roommate", () => {
     let roomHandler;
