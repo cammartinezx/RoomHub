103e8628ad33b954db3db91fa8d0a62644325f6a
WilliamOdumah <107610080+WilliamOdumah@users.noreply.github.com>
2024-10-08 03:05:25 -0500

    Added logo to landing page

--- Frontend/src/pages/LandingPage.js
+++ Frontend/src/pages/LandingPage.js
@@ -10,7 +10,7 @@ const LandingPage = () =>{
       <div className={styles.landingContainer}>
         <div className={styles.overlay}></div>
         <div className={styles.content}>
-          <img src="/logo.png" alt="RoomHub Logo" className={styles.logo} />
+          <img src="/logo2.png" alt="RoomHub Logo" className={styles.logo} />
           <h1 className={styles.heading}>Welcome To RoomHub</h1>
           <p className={styles.subheading}>Connecting people and spaces, seamlessly.</p>
           <button className={styles.loginButton} onClick={() => navigate('/login')}>Log In To Get Started</button>
