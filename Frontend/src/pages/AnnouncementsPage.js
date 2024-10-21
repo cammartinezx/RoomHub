f5c805b065de20cc3b076c00f91d5b3608629d9a
WilliamOdumah <107610080+WilliamOdumah@users.noreply.github.com>
2024-10-20 22:34:34 -0500

    can send announcements

--- Frontend/src/pages/AnnouncementsPage.js
+++ Frontend/src/pages/AnnouncementsPage.js
@@ -40,11 +40,13 @@ const AnnouncementsPage = () => {
         }
 
         try {
-            const response = await axios.post('https://api.example.com/send-announcement', {
+            const response = await axios.post('https://7hm4udd9s2.execute-api.ca-central-1.amazonaws.com/dev/notification/send-announcement', {
+                from: email,
                 message: messageToSend,
-                roomId: 'room123',  // Replace with dynamic room ID
+                type: 'announcement'
             });
             if (response.status === 200) {
+                alert('Announcement sent successfully!');
                 setFeedback('Announcement sent successfully!');
             } else {
                 setFeedback('Failed to send the announcement.');
