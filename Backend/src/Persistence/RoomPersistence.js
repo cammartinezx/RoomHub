c0d2f0b889d2dc7c7cf251abd438a966ac86f4b1
Hung Lu Dao <hungludao@gmail.com>
2024-10-28 15:52:05 -0500

    fix lint

--- Backend/src/Persistence/RoomPersistence.js
+++ Backend/src/Persistence/RoomPersistence.js
@@ -68,7 +68,7 @@ class RoomPersistence {
         // working_client = new DynamoDBClient(local_test_client);
 
         this.#doc_client = DynamoDBDocumentClient.from(working_client, {
-            marshallOptions: { convertEmptyValues: true }
+            marshallOptions: { convertEmptyValues: true },
         });
         this.#table_name = "room";
     }
