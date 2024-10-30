cbf95f393fa0e04b73a3c7420f7d1dd164c6fdfb
cammartinezx <camila.martinez2002@gmail.com>
2024-10-29 20:10:10 -0500

    added unit -tests for tasks

--- Backend/test/Unit-tests/Handler/task.test.js
+++ Backend/test/Unit-tests/Handler/task.test.js
@@ -8,25 +8,35 @@ const UserInfoHandler = require("../../../src/Handler/UserInfoHandler");
 
 jest.mock("../../../src/Utility/Services", () => ({
     get_user_persistence: () => ({
-        save_new_user: jest.fn(),
         get_user: jest.fn(),
-        get_notification: jest.fn(),
+        get_room_id: jest.fn(),
     }),
-
     get_room_persistence: () => ({
-        get_room_name: jest.fn(),
-    }),
-
-    get_notification_persistence: () => ({
-        get_msg_type: jest.fn(),
-        update_notification_status: jest.fn(),
+        add_task_to_room: jest.fn(),
+        delete_task_from_room:jest.fn(),
+        get_completed_tasks: jest.fn()
     }),
 
     get_task_persistence: () => ({
-        get_msg_type: jest.fn(),
-        update_notification_status: jest.fn(),
+        generate_new_task: jest.fn(),
+        get_task_by_id: jest.fn(),
+        update_task: jest.fn(),
+        delete_task: jest.fn()
+    }),
+    get_notification_persistence: () => ({
     }),
 }));
+
+jest.mock("../../../src/Handler/UserInfoHandler", () => {
+    return jest.fn().mockImplementation(() => ({
+        is_valid_user: jest.fn(),//.mockImplementation(() => true),
+        areRoommates: jest.fn(),//.mockImplementation(() => true),
+    }));
+});
+
+
+
+
 
 describe("Unit test for GET /", () => {
     let app;
@@ -40,7 +50,7 @@ describe("Unit test for GET /", () => {
         const res = await request(app).get("/");
 
         expect(res.status).toBe(200);
-        expect(res.body).toEqual({ Message: "Welcome to the Task paths" });
+        expect(res.body).toEqual({ message: "Welcome to the Task paths" });
     });
 });
 
@@ -48,14 +58,13 @@ describe("Unit test for creating a task", () => {
     let task_handler;
     let user_info_handler;
     let req, res;
-    let mock_is_valid_user;
-    let mock_are_roommates ;
+    let mock_get_room ;
     let mock_generate_new_task;
     let mock_add_task_to_room;
 
     beforeEach(() => {
-        task_handler = new TaskHandler();
         user_info_handler = new UserInfoHandler();
+        task_handler = new TaskHandler(user_info_handler);
         req = mockRequest();
         res = mockResponse();
 
@@ -63,11 +72,13 @@ describe("Unit test for creating a task", () => {
         res.json = jest.fn();
         jest.clearAllMocks();
 
-        // Mocking the validation functions
-         mock_is_valid_user = jest.spyOn(user_info_handler, "is_valid_user").mockResolvedValue(true);
-         mock_are_roommates = jest.spyOn(user_info_handler, "areRoommates").mockResolvedValue(true);
-         mock_generate_new_task = jest.spyOn(task_handler, "#task_persistence.generate_new_task").mockResolvedValue("SUCCESS");
-         mock_add_task_to_room = jest.spyOn(task_handler, "#room_persistence.add_task_to_room").mockResolvedValue("SUCCESS");
+        //Mocking the validation functions
+        mock_get_room = task_handler.get_user_persistence().get_room_id.mockImplementation((user_id) => "room1");
+        mock_generate_new_task = task_handler.get_task_persistence().generate_new_task.mockImplementation((unique_id, task_description, user_id, due_date) => {
+            return "SUCCESS" });
+        mock_add_task_to_room = task_handler.get_room_persistence().add_task_to_room.mockImplementation((room_id, task_id) => {
+            return "SUCCESS" });
+
     });
 
     afterEach(() => {
@@ -82,9 +93,9 @@ describe("Unit test for creating a task", () => {
             to: "user2@gmail.com",
             date: "2024-11-11",
         };
-
+        user_info_handler.is_valid_user.mockImplementation(() =>  true);
+        user_info_handler.areRoommates.mockImplementation(() => true);
         await task_handler.create_task(req, res);
-
         // Verify the response
         expect(res.status).toHaveBeenCalledWith(200);
         expect(res.json).toHaveBeenCalledWith({ message: "Task created successfully" });
@@ -98,7 +109,7 @@ describe("Unit test for creating a task", () => {
             date: "2024-11-11",
         };
 
-        mock_is_valid_user.mockResolvedValue(false);
+        user_info_handler.is_valid_user.mockImplementation(() => false);
 
         // Call create_user
         await task_handler.create_task(req, res);
@@ -116,7 +127,9 @@ describe("Unit test for creating a task", () => {
             to: "user2@gmail.com",
             date: "2024-11-11",
         };
-        mock_are_roommates.mockResolvedValue(false);
+
+        user_info_handler.is_valid_user.mockImplementation(() => true);
+        user_info_handler.areRoommates.mockImplementation(() => false);
         // Call create_user
         await task_handler.create_task(req, res);
         // Check response status and message
@@ -133,6 +146,10 @@ describe("Unit test for creating a task", () => {
             to: "user2@gmail.com",
             date: "2004-11-11",
         };
+
+        user_info_handler.is_valid_user.mockImplementation(() => true);
+        user_info_handler.areRoommates.mockImplementation(() => true);
+
         // Call create_user
         await task_handler.create_task(req, res);
         // Check response status and message
@@ -150,16 +167,19 @@ describe("Unit test for creating a task", () => {
             date: "2024-11-11",
         };
 
-        // Mock generate_new_task to return a success response
-        const create_task_mock = task_handler.#task_persistence.generate_new_task;
-        create_task_mock.mockResolvedValue(new Error("Error message from backend"));
+        user_info_handler.is_valid_user.mockImplementation(() => true);
+        user_info_handler.areRoommates.mockImplementation(() => true);
+
+        mock_generate_new_task.mockImplementation((user_id) => {
+            throw new Error("Something has occurred");
+        });
 
         // Call create_task
         await task_handler.create_task(req, res);
 
         // Verify the response
         expect(res.status).toHaveBeenCalledWith(500);
-        expect(res.json).toHaveBeenCalledWith({ message: "Something went wrong" });
+        expect(res.json).toHaveBeenCalledWith({ message: "An error occurred while creating the task" });
     });
 });
 
@@ -167,15 +187,15 @@ describe("Unit test for editing a task", () => {
     let task_handler;
     let user_info_handler;
     let req, res;
-    let mock_is_valid_user;
-    let mock_are_roommates ;
-    let mock_generate_new_task;
+    let mock_get_room;
+    let mock_edit_task;
     let mock_add_task_to_room;
     let mock_task_exists;
 
+
     beforeEach(() => {
-        task_handler = new TaskHandler();
         user_info_handler = new UserInfoHandler();
+        task_handler = new TaskHandler(user_info_handler);
         req = mockRequest();
         res = mockResponse();
 
@@ -184,39 +204,46 @@ describe("Unit test for editing a task", () => {
         jest.clearAllMocks();
 
         // Mocking the validation functions
-        mock_is_valid_user = jest.spyOn(user_info_handler, "is_valid_user").mockResolvedValue(true);
-        mock_are_roommates = jest.spyOn(user_info_handler, "areRoommates").mockResolvedValue(true);
-        mock_generate_new_task = jest.spyOn(task_handler, "#task_persistence.generate_new_task").mockResolvedValue("SUCCESS");
-        mock_add_task_to_room = jest.spyOn(task_handler, "#room_persistence.add_task_to_room").mockResolvedValue("SUCCESS");
-        mock_task_exists = jest.spyOn(task_handler.#task_persistence, "get_task_by_id").mockResolvedValue("SUCCESS");
+        mock_edit_task = task_handler.get_task_persistence().update_task.mockImplementation((unique_id, task_description, user_id, due_date) => {
+            return "SUCCESS" });
+        mock_get_room = task_handler.get_user_persistence().get_room_id.mockImplementation((user_id) => "room1");
+        mock_add_task_to_room = task_handler.get_room_persistence().add_task_to_room.mockImplementation((room_id, task_id) => {
+            return "SUCCESS" });
+        mock_task_exists = task_handler.get_task_persistence().get_task_by_id.mockImplementation((task_id) => "SUCCESS");
+
     });
 
     afterEach(() => {
         jest.restoreAllMocks();
     });
 
-    it("Send a success response verifying that the user was correctly created", async () => {
+    it("Send a success response verifying that the task was edited", async () => {
         // Setup valid request body
         req.body = {
+            id: "taskid",
             tn: "washing dishes",
             frm: "user1@gmail.com",
             to: "user2@gmail.com",
             date: "2024-11-11",
         };
+        user_info_handler.is_valid_user.mockImplementation(() => true);
+        user_info_handler.areRoommates.mockImplementation(() => true);
+
         await task_handler.edit_task(req, res);
         // Verify the response
         expect(res.status).toHaveBeenCalledWith(200);
-        expect(res.json).toHaveBeenCalledWith({ message: "Task created successfully" });
+        expect(res.json).toHaveBeenCalledWith({ message: "Task updated successfully" });
     });
     it("should return 403 when the user is invalid", async () => {
         // Setup invalid request body
         req.body = {
+            id: "taskid",
             tn: "washing dishes",
             frm: "user1@gmail.com",
             to: "user2@gmail.com",
             date: "2024-11-11",
         };
-        mock_is_valid_user.mockResolvedValue(false);
+        user_info_handler.is_valid_user.mockImplementation(() => false);
 
         // Call create_user
         await task_handler.edit_task(req, res);
@@ -229,12 +256,14 @@ describe("Unit test for editing a task", () => {
     it("should return 403 when the users are not roommates", async () => {
         // Setup invalid request body
         req.body = {
+            id: "taskid",
             tn: "washing dishes",
             frm: "user1@gmail.com",
             to: "user2@gmail.com",
             date: "2024-11-11",
         };
-        mock_are_roommates.mockResolvedValue(false);
+        user_info_handler.is_valid_user.mockImplementation(() => true);
+        user_info_handler.areRoommates.mockImplementation(() => false);
         // Call create_user
         await task_handler.edit_task(req, res);
         // Check response status and message
@@ -246,26 +275,16 @@ describe("Unit test for editing a task", () => {
     it("should return 403 when the date is invalid", async () => {
         // Setup invalid request body
         req.body = {
+            id: "taskid",
             tn: "Task 1",
             frm: "user1@gmail.com",
             to: "user2@gmail.com",
             date: "2004-11-11",
         };
-        // Call create_user
-        await task_handler.create_task(req, res);
-        // Check response status and message
-        expect(res.status).toHaveBeenCalledWith(403);
-        expect(res.json).toHaveBeenCalledWith({
-            message: "Invalid task name or due date",
-        });
-    });
-    it("should return 403 when the task is not found in the persistence", async () => {
-        req.body = {
-            tn: "Task 1",
-            frm: "user1@gmail.com",
-            to: "user2@gmail.com",
-            date: "2004-11-11",
-        };
+
+        user_info_handler.is_valid_user.mockImplementation(() => true);
+        user_info_handler.areRoommates.mockImplementation(() => true);
+
         // Call create_user
         await task_handler.create_task(req, res);
         // Check response status and message
@@ -277,36 +296,40 @@ describe("Unit test for editing a task", () => {
     it("should return 500 when an exception is thrown", async () => {
         // Setup valid request body
         req.body = {
+            id: "taskid",
             tn: "washing dishes",
             frm: "user1@gmail.com",
             to: "user2@gmail.com",
             date: "2024-11-11",
         };
 
-        // Mock generate_new_task to return a success response
-        const create_task_mock = task_handler.#task_persistence.generate_new_task;
-        create_task_mock.mockResolvedValue(new Error("Error message from backend"));
+        user_info_handler.is_valid_user.mockImplementation(() => true);
+        user_info_handler.areRoommates.mockImplementation(() => true);
+        mock_edit_task.mockImplementation((user_id) => {
+            throw new Error("Something has occurred");
+        });
 
         // Call create_task
-        await task_handler.create_task(req, res);
+        await task_handler.edit_task(req, res);
 
         // Verify the response
         expect(res.status).toHaveBeenCalledWith(500);
-        expect(res.json).toHaveBeenCalledWith({ message: "Something went wrong" });
+        expect(res.json).toHaveBeenCalledWith({ message: "An error occurred while updating the task" });
     });
 });
 
-describe("Unit test for delete a notification of specific user", () => {
+describe("Unit test for delete a task", () => {
     let task_handler;
     let user_info_handler;
     let req, res;
     let get_user_mock;
     let get_task_list_mock;
-    let delete_task_mock;
+    let delete_task_mock,mock_get_room;
 
     beforeEach(() => {
-        task_handler = new TaskHandler();
         user_info_handler = new UserInfoHandler();
+        task_handler = new TaskHandler(user_info_handler);
+
         req = mockRequest();
         res = mockResponse();
 
@@ -315,68 +338,65 @@ describe("Unit test for delete a notification of specific user", () => {
         jest.clearAllMocks();
 
         // Commonly used mocks
-        get_user_mock = jest.spyOn(user_info_handler, 'is_valid_user').mockResolvedValue(true);
-        get_task_list_mock = jest.spyOn(task_handler, 'get_pending_tasks_for_room').mockResolvedValue(["task1", "task2"]);
-        delete_task_mock = jest.spyOn(task_handler, 'delete_task_from_room').mockResolvedValue({ status: 200, message: "Task marked as completed" });
+        task_handler.get_user_persistence().get_room_id.mockImplementation((user_id) => "room1");
+        task_handler.get_task_persistence().get_task_by_id.mockImplementation((task_id) => "SUCCESS");
+        get_task_list_mock= task_handler.get_room_persistence().get_completed_tasks.mockImplementation((room_id) =>['task1', 'task3']);
+        task_handler.get_room_persistence().delete_task_from_room.mockImplementation(() => "SUCCESS");
+        delete_task_mock= task_handler.get_task_persistence().delete_task.mockImplementation(() => "SUCCESS");
     });
 
     afterEach(() => {
         jest.restoreAllMocks();
     });
 
-    it("should return 200 when task is successfully marked as completed", async () => {
+    it("should return 200 when task is successfully deleted", async () => {
         req.body = { id: "task1", frm: "user1@gmail.com" };
+
+        user_info_handler.is_valid_user.mockImplementation(() => true);
 
         await task_handler.delete_task(req, res);
 
         expect(res.status).toHaveBeenCalledWith(200);
-        expect(res.json).toHaveBeenCalledWith({ message: "Task marked as completed" });
+        expect(res.json).toHaveBeenCalledWith({ message: "Task deleted successfully" });
     });
 
     it("should return 403 when user is invalid", async () => {
         req.body = { id: "task1", frm: "user1@gmail.com" };
 
-        get_user_mock.mockResolvedValue(false); // Overwrite for this specific test
+        user_info_handler.is_valid_user.mockImplementation(() => false);
 
         await task_handler.delete_task(req, res);
 
-        expect(get_user_mock).toHaveBeenCalledTimes(1);
         expect(res.status).toHaveBeenCalledWith(403);
         expect(res.json).toHaveBeenCalledWith({ message: "Invalid user" });
     });
 
-    it("should return 403 when the task is not found in pending tasks", async () => {
+    it("should return 404 when the task is not found in completed tasks", async () => {
         req.body = { id: "task1", frm: "user1@gmail.com" };
 
-        get_task_list_mock.mockResolvedValue(['task2', 'task3']); // Adjust for this test
+        get_task_list_mock.mockImplementation((room_id) => ['task2', 'task3']);
+        user_info_handler.is_valid_user.mockImplementation(() => true);
+        // Adjust for this test
 
         await task_handler.delete_task(req, res);
 
-        expect(get_user_mock).toHaveBeenCalledTimes(1);
-        expect(get_task_list_mock).toHaveBeenCalledWith("task1");
-        expect(res.status).toHaveBeenCalledWith(403);
+        expect(res.status).toHaveBeenCalledWith(404);
         expect(res.json).toHaveBeenCalledWith({ message: "Task not found" });
     });
 
     it("should return 500 when an exception is thrown", async () => {
         req.body = { id: "task1", frm: "user1@gmail.com" };
 
-        delete_task_mock.mockRejectedValue(new Error("Error message from backend")); // Custom mock setup for this test
+        user_info_handler.is_valid_user.mockImplementation(() => true);
 
-        await task_handler.delete_task(req, res);
-
-        expect(delete_task_mock).toHaveBeenCalledTimes(1);
-        expect(res.status).toHaveBeenCalledWith(500);
-        expect(res.json).toHaveBeenCalledWith({ message: "Error message from backend" });
-    });
-    it("should return 500 when unable to retrieve room ID", async () => {
-        req.body = { id: "task1", frm: "user1@gmail.com" };
-        const get_user_mock = jest.spyOn(user_info_handler, 'is_valid_user').mockResolvedValue(true);
-        const get_room_id_mock = jest.spyOn(task_handler.#user_persistence, 'get_room_id').mockResolvedValue(null); // Simulate failure
+        delete_task_mock.mockImplementation((user_id) => {
+            throw new Error("Something has occurred");
+        });// Custom mock setup for this test
 
         await task_handler.delete_task(req, res);
 
         expect(res.status).toHaveBeenCalledWith(500);
         expect(res.json).toHaveBeenCalledWith({ message: "An error occurred while deleting the task" });
     });
+
 });
