67d78a87c0ad32c89f9e0490888628d386eb380f
DannyFabs <aderemifawoyed@gmail.com>
2024-11-04 10:40:55 -0600

    added responsive buttons for reuse and delete

--- flutter_frontend/lib/screens/TaskGrid.dart
+++ flutter_frontend/lib/screens/TaskGrid.dart
@@ -93,7 +93,6 @@ class _TaskGridState extends State<TaskGrid> {
                               tasks[index].dueDate,
                               style: TextStyle(color: theme.darkgrey, fontSize: 15),
                             ),
-                            const SizedBox(height: 10),
                             // is pending task type then return reuse and delete else return completed
                             widget.isPending ?
                             Center(
@@ -107,7 +106,7 @@ class _TaskGridState extends State<TaskGrid> {
                                 style: ElevatedButton.styleFrom(
                                   backgroundColor: theme.darkblue,
                                   //fixedSize: const Size(200, 30), // Minimum width and height
-                                  padding: const EdgeInsets.symmetric(horizontal: 40.0), // Padding
+                                  padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical:10), // Padding
                                 ), child: const Text("Completed",
                                   style: TextStyle(color: Colors.white, fontSize: 15),
                                 ),
@@ -126,7 +125,6 @@ class _TaskGridState extends State<TaskGrid> {
                                     style: TextStyle(color: Colors.white, fontSize: 15),
                                   ),
                                 ),
-                                // const SizedBox(width: 15,),
                                 ElevatedButton(onPressed: () { deletePressed(tasks[index].taskId, widget.userId, index);},
                                   style: ElevatedButton.styleFrom(
                                     backgroundColor: theme.darkblue,
