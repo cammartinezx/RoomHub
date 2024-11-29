05b5863b3215850602d0c313f21517ba152004de
Hung Lu Dao <hungludao@gmail.com>
2024-11-29 03:45:20 -0600

    fix almost bugs i saw in react, hopefully not get crash :))

--- Frontend/src/pages/ManageTasksPage.js
+++ Frontend/src/pages/ManageTasksPage.js
@@ -18,16 +18,19 @@ const ManageTasksPage = () => {
     const email = location.state?.email;
     const hasRoom = location.state?.hasRoom;
 
-     // Fetch roommates on component load
+    // Fetch roommates on component load
     useEffect(() => {
-        console.log("FETCHING ROOMMATES....")
         const fetchRoommates = async () => {
             try {
                 const response = await axios.get(`https://7hm4udd9s2.execute-api.ca-central-1.amazonaws.com/dev/user/${email}/get-user-roommates`);
-                console.log(response.data.users)
-                if (response.status === 200 && response.data.roommates) {
-                    console.log(response.data)
-                    setRoomMembers(response.data.roommates); // Set roommates in the state
+                if (response.status === 200 && response.data.all_roommates) {
+                    const filteredRoommates = response.data.all_roommates
+                        .map((roommate) => {
+                            const email = roommate[0];
+                            const name = roommate[1];
+                            return [ email, name ];
+                        });
+                    setRoomMembers(filteredRoommates);
                 }
             } catch (error) {
                 console.error("Error fetching roommates:", error.message);
@@ -130,7 +133,7 @@ const ManageTasksPage = () => {
                     date: formattedDate,
                 });
                 if(response.status === 200){
-                    alert(`Task assigned to ${newTask.assignee}`);
+                    alert(`Task assigned to ${newTask.assignee.split("@")[0]}`);
                     sendNotification(email, `A new task "${newTask.task}" has been created and assigned to ${newTask.assignee}.`);
                     window.location.reload();
                 }
@@ -179,7 +182,7 @@ const ManageTasksPage = () => {
                     id: editingTask.task_id,
                     tn: editingTask.task_description,
                     frm: email,
-                    to: editingTask.asignee,
+                    to: editingTask.asignee + "@gmail.com",
                     date: editingTask.due_date
                 });
                 alert('Task updated successfully!');
@@ -230,8 +233,8 @@ const ManageTasksPage = () => {
                     >
                         <option value="">Assign to Roommate</option>
                         {roomMembers.map((member, index) => (
-                            <option key={index} value={member}>
-                                {member}
+                            <option key={index} value={member[0]}>
+                                {member[1]}
                             </option>
                         ))}
                     </select>
@@ -308,8 +311,8 @@ const ManageTasksPage = () => {
                     >
                         <option value="">Assign to Roommate</option>
                         {roomMembers.map((member, index) => (
-                            <option key={index} value={member}>
-                                {member}
+                            <option key={index} value={member[0]}>
+                                {member[1]}
                             </option>
                         ))}
                     </select>
