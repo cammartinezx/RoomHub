6835fee4b91e9f201f95b0cf82fc1ac1801c8511
WilliamOdumah <107610080+WilliamOdumah@users.noreply.github.com>
2024-12-02 09:20:17 -0600

    Add files via upload

--- flutter_frontend/build/web/flutter_service_worker.js
+++ flutter_frontend/build/web/flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ad1333b98342e69ad1c161f85ed64450
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "0499576fced2a6e3f160d39add5b9308",
+"flutter_bootstrap.js": "e0236ad8b706264bea36d399b5f22250",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
